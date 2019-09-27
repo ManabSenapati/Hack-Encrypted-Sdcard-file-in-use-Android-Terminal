9963477ef5d3a72ca0143b124ea2d1e32e446a02
Manab Jyoti Bordoloi <senapatitanu@gmail.com>
2019-09-28 02:42:50 +0530

    list all files

---
+++ List_Dir.sh
@@ -0,0 +1,14 @@
+$ Hack-Encrypted-Sdcard-file-in-use Android-Terminal
+This is a Android file hack tool
+$ Simply run the sh file using any bash compiler and get all files name without unlock file manager and  other file services
+Programmer ManabJB
+$ git tool of hack encrypted sdcard
+file system
+echo "Listed Successfully--->"
+
+ls sdcard
+ls system
+ls system/sbin
+ls storage
+
+echo "hacking succes"
